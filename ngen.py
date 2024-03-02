4368f92aa7e53fc7aa64caa7e93657c99d9423d1
astavrop <artemstavropolov@gmail.com>
2024-03-02 18:06:58 +0100

    fix: change range of output numbers (less minuses).

--- ngen.py
+++ ngen.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     try:
         n = int(sys.argv[1])  # Get n from command-line argument
 
-        n_lst = list(range(-(n * 2), (n * 2)))
+        n_lst = list(range(int(-(n / 2)), (n * 2)))
         random.shuffle(n_lst)  # Shuffle the list in-place
         res_list = n_lst[:n]  # Take the first n elements
 
