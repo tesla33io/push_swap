660ee1237c5981cabbd98c715647d56eb4271fa2
astavrop <artemstavropolov@gmail.com>
2024-03-05 17:30:04 +0100

    fix: add error protection for `clsts_to_b` & `clsts_to_t`.

--- src/algo/transfer.c
+++ src/algo/transfer.c
@@ -6,13 +6,14 @@
 /*   By: astavrop <astavrop@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 14:46:39 by astavrop          #+#    #+#             */
-/*   Updated: 2024/03/04 20:33:20 by astavrop         ###   ########.fr       */
+/*   Updated: 2024/03/05 17:28:23 by astavrop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "push_swap.h"
 #include <limits.h>
+#include <stdlib.h>
 
 /**
  * @brief  Calculates the position of the node with the closest value to the top of the target stack.
@@ -28,6 +29,11 @@ int	clsts_to_t(t_deque *trgt, int val)
 	int				i;
 	t_deque_node	*cur;
 
+	if (!trgt || !trgt->head)
+	{
+		ft_printf(2, "[transfer.c:clsts_to_t] Error.\n");
+		return (-1);
+	}
 	i = 0;
 	cur = trgt->head;
 	clst = INT_MAX;
@@ -51,13 +57,18 @@ int	clsts_to_t(t_deque *trgt, int val)
  * @param  val: value that will be transferred to the target stack
  * @retval position of the closest node (by value) to the bottom
  */
-int	closestToBottomPosition(t_deque *trgt, int val)
+int	clsts_to_b(t_deque *trgt, int val)
 {
 	int				pos;
 	int				closest;
 	int				i;
 	t_deque_node	*cur;
 
+	if (!trgt || !trgt->head)
+	{
+		ft_printf(2, "[transfer.c:clsts_to_b] Error.\n");
+		return (-1);
+	}
 	i = 0;
 	cur = trgt->head->prev;
 	closest = INT_MAX;
@@ -82,14 +93,43 @@ void	transfer(t_deque *a, t_deque *b, t_deque_type val)
 	int		back_in_a;
 
 	steps = clsts_to_t(a, val);
+	if (steps < 0)
+		return ;
 	s_in_b = deque_get_index(b, val);
 	s_in_a = steps - s_in_b;
 	back_in_a = s_in_a;
-	// while (s_in_a-- && s_in_b--)
-	// 	do_rr(a, b, true);
 	while (s_in_b-- > 0)
 		do_rotate(b, true, RB);
-	// while (s_in_a-- > 0)
-	// 	do_rotate(a, true, RA);
+	while (s_in_a-- > 0)
+		do_rotate(a, true, RA);
 	do_push(b, a, true, PA);
+}
+
+void	move_head(t_deque *from, t_deque *to)
+{
+	int		ttop;
+	int		tbtm;
+	int		i;
+
+	ttop = clsts_to_t(to, from->head->data);
+	tbtm = clsts_to_b(to, from->head->data);
+	if (ttop < 0 || tbtm < 0)
+		exit(-1);
+	ft_printf(1, "top: %d - btm: %d\n", ttop, tbtm);
+	if (ttop < tbtm)
+	{
+		i = 0;
+		while (i < ttop - 1)
+			do_rotate(to, true, RB);
+		do_push(from, to, true, PB);
+		// do rb
+	}
+	else
+	{
+		i = 0;
+		while (i < tbtm - 1)
+			do_reverse_rotate(to, true, RRB);
+		do_push(from, to, true, PB);
+		// do rrb
+	}
 }
