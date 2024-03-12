997bee5de2fcdee10e4b7ee21246a7e1e6ce5d47
astavrop <artemstavropolov@gmail.com>
2024-03-12 21:46:35 +0100

    feat: improve test script.

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ fi
 n=$1
 
 # Run the Python script to get random numbers
-python_script_output=$(python3 ngen.py "$n")
+python_script_output=$(seq 1 "$n" | shuf | tr "\n" " ")
 
 # Run make to update the bin if anything changed
 # and run push_swap with random numbers after
@@ -18,7 +18,11 @@ echo -ne "\033[31m*******************************************************"
 echo -e "************************************************\033[0m\n"
 
 make
-./push_swap $python_script_output
-echo -e "Used command: ./push_swap $python_script_output"
+# ./push_swap $python_script_output
+echo -e "\033[36;1mChecker result\t| Ops count" &
+echo -en $(./push_swap $python_script_output | ./checker_linux $python_script_output)"\t\t| " &
+echo $(sleep 0.1 && ./push_swap $python_script_output | wc -l) &
+wait
+echo -e "\033[0mUsed command: \033[33m./push_swap $python_script_output\033[0m"
 echo -ne "\n\033[31m*******************************************************"
 echo -e "************************************************\033[0m"
