79f0c65ab516e8f85c142bab3858ed997dd7eaf7
astavrop <artemstavropolov@gmail.com>
2024-03-13 16:30:24 +0100

    fix: refactoring, handle 3 numbers case.

--- src/main.c
+++ src/main.c
@@ -16,6 +16,19 @@
 
 #include "../include/operations.h"
 
+static void	a_small_check(t_deque *a)
+{
+	if (a->size == 2 && is_sorted(a))
+		exit(ps_clear(a, NULL));
+	if (a->size == 2 && !is_sorted(a))
+	{
+		do_swap(a, true, SA);
+		exit(ps_clear(a, NULL));
+	}
+	if (is_sorted(a))
+		exit(ps_clear(a, NULL));
+}
+
 int	main(int ac, char **av)
 {
 	t_deque	*a;
@@ -23,15 +36,15 @@ int	main(int ac, char **av)
 
 	check_input(ac, av);
 	a = parse_input(ac, av);
-	deque_print(a);
-	if (a->size == 2 && !is_sorted(a))
-	{
-		do_swap(a, true, SA);
-		exit(0);
-	}
+	a_small_check(a);
 	b = malloc(sizeof(t_deque));
 	b->head = NULL;
 	b->size = 0;
+	if (a->size == 3 && !is_sorted(a))
+	{
+		sort_three(a);
+		exit (ps_clear(a, b));
+	}
 	prepare_b(a, b);
 	fill_b(a, b);
 	sort_three(a);
